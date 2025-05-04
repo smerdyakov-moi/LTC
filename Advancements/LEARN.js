27f74bbdf53e709c38594f22507a7d0011406ecf
smerdyakov-moi <pragyankhati45@gmail.com>
2025-05-04 10:20:29 +0545

    ArrDotMethod

--- Advancements/LEARN.js
+++ Advancements/LEARN.js
@@ -8,4 +8,4 @@ new_arr=arr.filter(function(i){ // arr.map is only used to directly copy the ele
     //arr.find to return the first item you're looking for. Will return undefined if nothing found
      if(i>2){return i}
 })
-console.log(new_arr)
+console.log(arr.indexOf(3)) //Index of 
