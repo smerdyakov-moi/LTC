c9c986be2e2036b8748d63c6985a417d039bd7f3
smerdyakov-moi <pragyankhati45@gmail.com>
2025-05-04 10:16:09 +0545

    ArrDotMethod

--- Advancements/LEARN.js
+++ Advancements/LEARN.js
@@ -5,6 +5,7 @@ arr.forEach(function(val){
 
 let new_arr=[]
 new_arr=arr.filter(function(i){ // arr.map is only used to directly copy the elememnts or manipulate 
+    //arr.find to return the first item you're looking for. Will return undefined if nothing found
      if(i>2){return i}
 })
 console.log(new_arr)
