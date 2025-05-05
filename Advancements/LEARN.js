f0f25346f7e3ebf5e54ee24b1e1d9a7db40e3dd2
smerdyakov-moi <pragyankhati45@gmail.com>
2025-05-05 10:59:44 +0545

    FileHandling

--- Advancements/LEARN.js
+++ Advancements/LEARN.js
@@ -1,5 +1,6 @@
 const fs = require('node:fs'); 
 const { constants } = require('node:fs');
 
-fs.readFile(".\\happy\\jsr.txt", 'utf8', function(err, data) {});
+var content=fs.readFileSync(".\\happy\\jr.txt", 'utf8', function(err, data) {
+});
 console.log(content)
