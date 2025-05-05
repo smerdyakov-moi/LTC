0b6f26cc9370886899940c6bd9cfa564bd50c722
smerdyakov-moi <pragyankhati45@gmail.com>
2025-05-05 18:47:01 +0545

    NodeNpmoduleexpress@4.18.2

--- Advancements/LEARN.js
+++ Advancements/LEARN.js
@@ -1,6 +1,24 @@
-const fs = require('node:fs'); 
-const { constants } = require('node:fs');
+import express from 'express'
 
-var content=fs.readFileSync(".\\happy\\jr.txt", 'utf8', function(err, data) {
-});
-console.log(content)
+const app = express()
+
+//Middleware
+app.use((req,res,next)=>{
+    console.log("Middleware 1")
+    next()
+})
+
+app.get('/',(req,res,next)=>{
+    res.send('Welcome to this page. Next page will reveal my name. ')
+})
+app.get('/profile', (req, res,next) => {
+    return next(new Error('Something went wrong'))
+  })
+
+//Error handler
+app.use((err,req,res,next)=>{
+    console.error(err.stack)
+    res.status(404).send("Something broke!")
+})
+
+app.listen(3000)
