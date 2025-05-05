a324fbdece92f5340cfbe0fedecf01e7ffb01a9f
smerdyakov-moi <pragyankhati45@gmail.com>
2025-05-05 09:57:00 +0545

    Advs

--- Advancements/LEARN.js
+++ Advancements/LEARN.js
@@ -1,11 +1,4 @@
-let arr = [1,2,3,4];
-arr.forEach(function(val){
-    console.log(val+"hello")
+const fs = require('node:fs'); 
+fs.rename('2.txt','intro.txt', function(err){
+    (err)? console.error(err) : console.log("done")
 })
-
-let new_arr=[]
-new_arr=arr.filter(function(i){ // arr.map is only used to directly copy the elememnts or manipulate 
-    //arr.find to return the first item you're looking for. Will return undefined if nothing found
-     if(i>2){return i}
-})
-console.log(arr.indexOf(3)) //Index of 
